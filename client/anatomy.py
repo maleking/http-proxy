7b6cd2a526b025b48e97be9e22d6cccf76519f7d
Akrez <akrez.like@gmail.com>
2024-12-26 01:49:25 +0330

    anatomy.py

--- client/anatomy.py
+++ client/anatomy.py
@@ -19,11 +19,13 @@ def request(flow: HTTPFlow):
     new_host        = new_uri.hostname
     new_port        = new_uri.port if new_uri.port else 443 if new_scheme == "https" else 80 
     new_path_prefix = new_uri.path
+    
+    # print(new_scheme, new_host, new_port, new_path_prefix)
 
     old_http_version = str(int(float(flow.request.http_version.replace("HTTP/", ""))*10))
 
     # print(assemble_request(flow.request))
-    print("["+strftime("%H:%M:%S", gmtime()) + "] " + flow.request.http_version.ljust(9, ' ') + flow.request.method.ljust(8, ' ') + flow.request.host)
+    # print("["+strftime("%H:%M:%S", gmtime()) + "] " + flow.request.http_version.ljust(9, ' ') + flow.request.method.ljust(8, ' ') + flow.request.host)
 
     flow.request.path           = new_path_prefix + "?" + flow.request.method + "_" +  flow.request.scheme + "_" + old_http_version + "/" + (flow.request.host) + (flow.request.path)
     flow.request.method         = "POST"
@@ -33,6 +35,9 @@ def request(flow: HTTPFlow):
     flow.request.port           = new_port
 
     flow.request.headers["host"] = new_host
+
+    # print(assemble_request(flow.request))
+    # print(flow.request.url)
 
 def response(flow: http.HTTPFlow):
     # print("["+strftime("%H:%M:%S", gmtime()) + "] " + str(flow.response.status_code) + " " + flow.request.scheme + "://" + (flow.request.host) + (flow.request.path) + "\r\n")
