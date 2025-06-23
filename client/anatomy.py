dcec6d1fc829bc9f4b90a1837a4cc3f5d075e708
Akrez <akrez.like@gmail.com>
2025-06-23 16:21:33 +0330

    up

--- client/anatomy.py
+++ client/anatomy.py
@@ -10,34 +10,30 @@ from mitmproxy.http import HTTPFlow
 from mitmproxy import version
 from urllib.parse import urlparse
 from time import gmtime, strftime
+import configparser
 
 def request(flow: HTTPFlow):
 
-    new_uri = urlparse("https://agent.akrezing.ir/index.php")
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    new_uri = urlparse(config['agent']['url'])
+    new_host_header = config['agent']['host_header']
 
-    new_scheme      = new_uri.scheme
-    new_host        = new_uri.hostname
-    new_port        = new_uri.port if new_uri.port else 443 if new_scheme == "https" else 80 
-    new_path_prefix = new_uri.path
-    
-    # print(new_scheme, new_host, new_port, new_path_prefix)
-
+    new_scheme       = new_uri.scheme
+    new_port         = new_uri.port if new_uri.port else 443 if new_scheme == "https" else 80 
+    old_host         = flow.request.host
     old_http_version = str(int(float(flow.request.http_version.replace("HTTP/", ""))*10))
 
+    flow.request.path            = new_uri.path + "/" + flow.request.method + "_" +  flow.request.scheme + "_" + old_http_version + "/" + (flow.request.host) + (flow.request.path)
+    flow.request.method          = "POST"
+    flow.request.http_version    = "HTTP/2.0"
+    flow.request.scheme          = new_scheme
+    flow.request.host            = new_uri.hostname
+    flow.request.port            = new_port
+    flow.request.headers["host"] = new_host_header
+
+    print("["+strftime("%H:%M:%S", gmtime()) + "] " + flow.request.http_version.ljust(9, ' ') + flow.request.method.ljust(8, ' ') + old_host)
     # print(assemble_request(flow.request))
-    # print("["+strftime("%H:%M:%S", gmtime()) + "] " + flow.request.http_version.ljust(9, ' ') + flow.request.method.ljust(8, ' ') + flow.request.host)
-
-    flow.request.path           = new_path_prefix + "/" + flow.request.method + "_" +  flow.request.scheme + "_" + old_http_version + "/" + (flow.request.host) + (flow.request.path)
-    flow.request.method         = "POST"
-    flow.request.http_version   = "HTTP/2.0"
-    flow.request.scheme         = new_scheme
-    flow.request.host           = new_host
-    flow.request.port           = new_port
-
-    flow.request.headers["host"] = new_host
-
-    # print(assemble_request(flow.request))
-    # print(flow.request.url)
 
 def response(flow: http.HTTPFlow):
     # print("["+strftime("%H:%M:%S", gmtime()) + "] " + str(flow.response.status_code) + " " + flow.request.scheme + "://" + (flow.request.host) + (flow.request.path) + "\r\n")
