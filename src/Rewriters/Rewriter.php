70fe4f46d3fe328d705b88da8e8efe9060b996f9
Akrez <akrez.like@gmail.com>
2025-08-15 15:16:48 +0330

    Rewriter

---
+++ src/Rewriters/Rewriter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Akrez\HttpProxy\Rewriters;
+
+use Akrez\HttpProxy\Senders\RewriteSender;
+use Psr\Http\Message\ResponseInterface;
+
+abstract class Rewriter
+{
+    public function __construct(protected RewriteSender $rewriteSender) {}
+
+    abstract public function convert($body, $mainPageUrl);
+
+    abstract public function isMine($response);
+
+    public function encryptUrl(string $urlString, ?string $mainUrlString = null)
+    {
+        return $this->rewriteSender->encryptUrl($urlString, $mainUrlString);
+    }
+
+    public static function isContentType(string $contentType, ResponseInterface $response)
+    {
+        $contentTypes = (array) $response->getHeader('Content-Type');
+
+        return $contentType === trim(preg_replace('@;.*@', '', reset($contentTypes)));
+    }
+
+    public static function trim(string $url)
+    {
+        return trim($url, " \n\r\t\v\0/");
+    }
+
+    public static function startsWith($haystack, $needles)
+    {
+        foreach ((array) $needles as $needle) {
+            if ($needle !== '' && stripos($haystack, $needle) === 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
