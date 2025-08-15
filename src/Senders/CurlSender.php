c6741d6985473b678fa1b30d897ff1b99d72eed2
Akrez <akrez.like@gmail.com>
2025-08-15 23:43:33 +0330

    up

--- src/Senders/CurlSender.php
+++ src/Senders/CurlSender.php
@@ -2,10 +2,56 @@
 
 namespace Akrez\HttpProxy\Senders;
 
+use GuzzleHttp\Psr7\Message;
 use Psr\Http\Message\RequestInterface;
 
-class CurlSender extends Sender
+class CurlSender
 {
+    protected ?int $timeout = null;
+
+    protected int $bufferSize = 128;
+
+    protected bool $debug = false;
+
+    public function setTimeout(?int $timeout): self
+    {
+        $this->timeout = $timeout;
+
+        return $this;
+    }
+
+    public function setBufferSize(?int $bufferSize): self
+    {
+        $this->bufferSize = $bufferSize;
+
+        return $this;
+    }
+
+    public function setDebug(bool $debug): self
+    {
+        $this->debug = $debug;
+
+        return $this;
+    }
+
+    public function emit(RequestInterface $newRequest)
+    {
+        $newRequest = $newRequest
+            ->withoutHeader('Accept-Encoding')
+            ->withHeader('Accept-Encoding', 'identity');
+
+        if ($this->debug) {
+            echo nl2br(Message::toString($newRequest));
+        } elseif ($newRequest->getMethod() === 'CONNECT') {
+            header('Content-Type: application/json; charset=utf-8', true, 200);
+            echo json_encode([
+                'status' => 200,
+            ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        } else {
+            $this->emitRequest($newRequest);
+        }
+    }
+
     protected function emitRequest(RequestInterface $newRequest)
     {
         $url = (string) $newRequest->getUri();
@@ -52,7 +98,7 @@ class CurlSender extends Sender
         return true;
     }
 
-    protected function headerCallback($ch, $headers)
+    private function headerCallback($ch, $headers)
     {
         $parts = explode(':', $headers, 2);
 
@@ -77,7 +123,7 @@ class CurlSender extends Sender
         return strlen($headers);
     }
 
-    protected function writeCallback($ch, $str)
+    private function writeCallback($ch, $str)
     {
         $len = strlen($str);
 
