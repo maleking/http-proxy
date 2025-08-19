acab024afc1f11507ad128003c026ae83165c2ac
Akrez <akrez.like@gmail.com>
2025-08-20 01:51:44 +0330

    up

--- index.php
+++ index.php
@@ -2,16 +2,10 @@
 
 require_once './vendor/autoload.php';
 
-use Akrez\HttpProxy\Factories\InbodyFactory;
 use Akrez\HttpProxy\Factories\InlineFactory;
 use Akrez\HttpProxy\Senders\CurlSender;
 
 function inline()
 {
     return InlineFactory::emitSender(new CurlSender);
-}
-
-function inbody()
-{
-    return InbodyFactory::emitSender(new CurlSender);
 }
