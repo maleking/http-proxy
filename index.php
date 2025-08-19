6d7fd348e4ba3a2ce475bf199936e1c39e78cf4f
Akrez <akrez.like@gmail.com>
2025-08-20 01:08:27 +0330

    up

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
+require_once './vendor/autoload.php';
+
 use Akrez\HttpProxy\Factories\InlineFactory;
 use Akrez\HttpProxy\Senders\CurlSender;
 
-$newRequest = InlineFactory::emitSender(new CurlSender());
+$newRequest = InlineFactory::emitSender(new CurlSender);
